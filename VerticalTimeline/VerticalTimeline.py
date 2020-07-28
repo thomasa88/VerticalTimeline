7cc7229be6825eeb3f17574b0703fd8eb392189b
Thomas Axelsson <thomasa88@gmail.com>
2020-07-28 12:13:37 +0200

    License

--- VerticalTimeline/VerticalTimeline.py
+++ VerticalTimeline/VerticalTimeline.py
@@ -1,6 +1,25 @@
 #Author-Thomas Axelsson
 #Description-Provides a vertical timeline.
 
+# This file is part of VerticalTimeline, a Fusion 360 add-in that
+# provides a vertical timeline.
+#
+# Copyright (C) 2020  Thomas Axelsson
+#
+# VerticalTimeline is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# VerticalTimeline is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with VerticalTimeline.  If not, see <https://www.gnu.org/licenses/>.
+
+
 # Put add-in folder in %appdata%\Autodesk\Autodesk Fusion 360\API\AddIns
 
 # Need to use Visual Studio Code Python extension version 2019.9.34911
